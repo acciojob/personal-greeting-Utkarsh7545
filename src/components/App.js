6aae2a82d5f2f831bd826569a8e0c1b5c41e45d5
Utkarsh7545 <utkarshkumar7545@gmail.com>
2023-06-15 15:40:57 +0530

    submitting code-Thu Jun 15 2023 15:40:55 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
 import React from "react";
+import {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [name, setName] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+        <label for="name" >Enter your name:</label><br/>
+        <input id="name" type="text" onChange={(event)=> {setName(event.target.value)}} /> {/* onChange is a pre-defined function in JS, for instant change a content*/}
+        <p>{name && `Hello ${name}!`}</p>
     </div>
   )
 }
